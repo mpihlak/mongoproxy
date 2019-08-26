5ea1204b504386291e83cc91772694c8ee9876b8
Martin Pihlak <martin.pihlak@starship.co>
2019-08-26 20:58:14 +0300

    Remove unused code from modules

--- src/mongodb/messages.rs
+++ src/mongodb/messages.rs
@@ -2,11 +2,10 @@ use byteorder::{LittleEndian, ReadBytesExt};
 use std::io::{self, Read, Error, ErrorKind};
 use bson::{decode_document};
 use std::fmt;
-use log::{debug,info,warn};
+use log::{info};
 
 
 pub const HEADER_LENGTH: usize = 16;
-
 
 #[derive(Debug)]
 pub struct MsgHeader {
@@ -45,12 +44,11 @@ pub struct MsgOpMsg {
 impl fmt::Display for MsgOpMsg {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "OP_MSG flags: {}, kind: {}\n",
-               self.flag_bits, self.kind).unwrap();
-
+               self.flag_bits, self.kind)?;
         for (i, v)  in self.sections.iter().enumerate() {
             write!(f, "section {}: {}\n", i, v)?;
         }
-        write!(f, ".")
+        Ok(())
     }
 }
 
@@ -114,102 +112,3 @@ fn read_c_string(rdr: impl Read) -> io::Result<String> {
 
     Err(Error::new(ErrorKind::Other, "conversion error"))
 }
-
-pub struct MongoProtocolParser {
-    header: MsgHeader,
-    have_header: bool,
-    want_bytes: usize,      // How many more bytes do we need for a complete message
-    message_buf: Vec<u8>,   // Accumulated message bytes, parseable when we have all want_bytes
-}
-
-impl MongoProtocolParser {
-
-    pub fn new() -> MongoProtocolParser {
-        MongoProtocolParser{
-            header: MsgHeader::new(),
-            have_header: false,
-            want_bytes: HEADER_LENGTH,
-            message_buf: Vec::new(),
-        }
-    }
-
-    // Parse the buffer and advance the internal state
-    //
-    // The buffer that is passed to parsing is a segment from a stream
-    // of bytes, so we try to assemble this into a complete message and
-    // parse that.
-    //
-    // The first message we always want to see is the MongoDb message header.
-    // This header in turn contains the length of the message that follows. So
-    // we try to read message length worth of bytes and parse the message. Once
-    // the message is parsed we expect a header again and the process repeats.
-    //
-    // TODO: Shut down the parser on parsing errors
-    // TODO: Return parsing result
-    //
-    pub fn parse_buffer(&mut self, buf: &Vec<u8>) {
-        let mut work_buf = &buf[..];
-
-        loop {
-            self.message_buf.extend(work_buf.iter().take(self.want_bytes));
-
-            if work_buf.len() < self.want_bytes {
-                self.want_bytes -= work_buf.len();
-                return;
-            }
-
-            // We have some surplus data, we'll get to that in the next loops
-            work_buf = &work_buf[self.want_bytes..];
-
-            debug!("bytes in buffer: {}, bytes in message: {}, want: {}",
-                     work_buf.len(), self.message_buf.len(), self.want_bytes);
-
-            if !self.have_header {
-                match MsgHeader::from_reader(&self.message_buf[..]) {
-                    Ok(header) => {
-                        debug!("parse: got a header: {:?}", header);
-                        assert!(header.message_length >= HEADER_LENGTH);
-
-                        self.header = header;
-                        self.have_header = true;
-                        self.want_bytes = self.header.message_length - HEADER_LENGTH;
-                        debug!("want {} more bytes", self.want_bytes);
-                    },
-                    Err(e) => {
-                        warn!("parse: failed to read a header: {}", e);
-                    },
-                }
-            } else {
-                debug!("processing payload {} bytes", self.header.message_length - HEADER_LENGTH);
-
-                match self.header.op_code {
-                    2004 => {
-                        let op = MsgOpQuery::from_reader(&self.message_buf[..]);
-                        info!("OP_QUERY: {:?}", op);
-                    },
-                    2013 => {
-                        let op = MsgOpMsg::from_reader(&self.message_buf[..]);
-                        info!("OP_MSG: {}", op.unwrap());
-                    },
-                    op_code => {
-                        info!("OP {}", op_code);
-                    },
-                }
-
-                if work_buf.len() > self.want_bytes {
-                    work_buf = &work_buf[self.want_bytes..];
-                }
-
-                self.have_header = false;
-                self.want_bytes = HEADER_LENGTH;
-            }
-
-            // At this point we've finished parsing of a complete buffer, but still
-            // have some surplus to deal with. Start afresh with an empty buffer and
-            // go for another loop.
-            //
-            self.message_buf.clear();
-        }
-    }
-}
-
