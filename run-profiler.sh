28dc4420949405d09d6850eae0ff4c9e85ae7782
Martin Pihlak <martin.pihlak@starship.co>
2019-09-24 10:28:39 +0300

    Profile the thing

---
+++ run-profiler.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PATH=$HOME/src/FlameGraph:$PATH
+
+echo "Starting dtrace"
+echo "Run the workload in another terminal, then come back here and ^C the dtrace."
+
+sudo dtrace -o tmp/out.stacks -n 'profile-997 /execname == "mongoproxy"/ { @[ustack(100)] = count(); }' \
+    -c 'target/debug/mongoproxy --hostport localhost:27017'
+
+echo "Generating flamegraph"
+OUTPUT=tmp/prof.svg
+
+stackcollapse.pl tmp/out.stacks | flamegraph.pl > $OUTPUT
+echo "Done, output in $OUTPUT"
