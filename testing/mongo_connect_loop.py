183a6b716964486c01688b705df8d9735cb82cf9
Martin Pihlak <martin.pihlak@starship.co>
2019-12-10 19:41:17 +0200

    Optionally pass an uri to the looper

--- testing/mongo_connect_loop.py
+++ testing/mongo_connect_loop.py
@@ -1,11 +1,23 @@
+import time
 import pymongo
+import sys, os
+
+if len(sys.argv) != 2:
+    mongo_uri = "mongodb://localhost:27111"
+else:
+    mongo_uri = sys.argv[1]
 
 count = 0
 while 1:
     count += 1
-    print "connection", count
-    con = pymongo.MongoClient("mongodb://localhost:27111")
+    print "#%d connecting to %s"  % (count, mongo_uri)
+    con = pymongo.MongoClient(mongo_uri)
 
     bigcollection = con['test']['bigcollection']
-    for res in bigcollection.find({}).limit(1000):
-        pass
+
+    start = time.time()
+    for i in range(1000):
+        for res in bigcollection.find({}).limit(10):
+            pass
+    elapsed = time.time() - start
+    print "%d queries in %.5f seconds, avg %.5f / sec" % (i, elapsed, i / elapsed)
