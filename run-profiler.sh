068d725c06996dbc503fbb430a062fc4bc5d1327
Martin Pihlak <martin.pihlak@starship.co>
2020-01-18 18:16:46 +0200

    Update profiler commandline

--- run-profiler.sh
+++ run-profiler.sh
@@ -6,7 +6,7 @@ echo "Starting dtrace"
 echo "Run the workload in another terminal, then come back here and ^C the dtrace."
 
 sudo dtrace -o tmp/out.stacks -n 'profile-997 /execname == "mongoproxy"/ { @[ustack(100)] = count(); }' \
-    -c 'target/debug/mongoproxy --hostport 127.0.0.1:27017'
+    -c 'target/release/mongoproxy --proxy 27111:127.0.0.1:27017'
 
 echo "Generating flamegraph"
 OUTPUT=tmp/prof.svg
