9a20d4b5b5b1fac0d66258a1d79e1ad8bb7ccc91
Martin Pihlak <martin.pihlak@starship.co>
2020-01-27 23:27:29 +0200

    Full tracing client again

--- testing/tracing_client.py
+++ testing/tracing_client.py
@@ -38,46 +38,45 @@ if __name__ == "__main__":
         print("Cleaning up")
         with tracer.start_span('Delete data', root_span) as span:
             span_text = span_as_text(span)
-            coll.delete_many({ "a": 1, "$comment": span_text })
+            coll.delete_many({ "$comment": span_text })
 
-        if 0:
-            print("Inserting")
-            with tracer.start_span('Insert data', root_span) as span:
-                span_text = span_as_text(span)
-                for i in range(110):
-                    # Insert does not actually take a $comment so we just skip
-                    # it's not that interesting op anyway.
-                    coll.insert({"name": "Purry", "number": i })
+        print("Inserting")
+        with tracer.start_span('Insert data', root_span) as span:
+            span_text = span_as_text(span)
+            for i in range(110):
+                # Insert does not actually take a $comment so we just skip
+                # it's not that interesting op anyway.
+                coll.insert({"name": "Purry", "number": i })
 
-            print("Fetching exactly 101")
-            with tracer.start_span('Fetching 101', root_span) as span:
-                span_text = span_as_text(span)
-                # Note: this uses limit() which is not exactly the same as fetching
-                # from a collection that has exactly 101 documents. In that case the
-                # "find" would actually be followed by empty "getMore"
-                for r in coll.find({}).comment(span_text).limit(101):
-                    pass
+        print("Fetching exactly 101")
+        with tracer.start_span('Fetching 101', root_span) as span:
+            span_text = span_as_text(span)
+            # Note: this uses limit() which is not exactly the same as fetching
+            # from a collection that has exactly 101 documents. In that case the
+            # "find" would actually be followed by empty "getMore"
+            for r in coll.find({}).comment(span_text).limit(101):
+                pass
 
-            print("Fetching All")
-            with tracer.start_span('Fetching All', root_span) as span:
-                span_text = span_as_text(span)
-                for r in coll.find({}).comment(span_text):
-                    pass
+        print("Fetching All")
+        with tracer.start_span('Fetching All', root_span) as span:
+            span_text = span_as_text(span)
+            for r in coll.find({}).comment(span_text):
+                pass
 
-            print("Fetching with batch size")
-            with tracer.start_span('Small batch fetch', root_span) as span:
-                span_text = span_as_text(span)
-                for r in coll.find({}).batch_size(50).comment(span_text):
-                    pass
+        print("Fetching with batch size")
+        with tracer.start_span('Small batch fetch', root_span) as span:
+            span_text = span_as_text(span)
+            for r in coll.find({}).batch_size(50).comment(span_text):
+                pass
 
-            print("Aggregate query")
-            with tracer.start_span('Aggregate fetch', root_span) as span:
-                span_text = span_as_text(span)
-                for r in coll.aggregate([
-                    { "$match": { "name": "Purry", "$comment": span_text } },
-                    { "$group": { "_id": "$number", "total": { "$sum": "$number" } } },
-                    { "$sort": { "total": -1 } } ]):
-                    pass
+        print("Aggregate query")
+        with tracer.start_span('Aggregate fetch', root_span) as span:
+            span_text = span_as_text(span)
+            for r in coll.aggregate([
+                { "$match": { "name": "Purry", "$comment": span_text } },
+                { "$group": { "_id": "$number", "total": { "$sum": "$number" } } },
+                { "$sort": { "total": -1 } } ]):
+                pass
 
     time.sleep(2)
     tracer.close()
