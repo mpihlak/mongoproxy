cbce86d4f6eeebeaef0cd32e798bc1288427c756
Martin Pihlak <martin.pihlak@starship.co>
2020-05-13 18:11:29 +0300

    Remove stacks before profiler run

--- run-profiler.sh
+++ run-profiler.sh
@@ -5,11 +5,13 @@ export PATH=$HOME/src/FlameGraph:$PATH
 echo "Starting dtrace"
 echo "Run the workload in another terminal, then come back here and ^C the dtrace."
 
-sudo dtrace -o tmp/out.stacks -n 'profile-997 /execname == "mongoproxy"/ { @[ustack(100)] = count(); }' \
+stacks=tmp/out.stacks
+rm -f $stacks
+sudo dtrace -o $stacks -n 'profile-997 /execname == "mongoproxy"/ { @[ustack(100)] = count(); }' \
     -c 'target/release/mongoproxy --proxy 27111:127.0.0.1:27017'
 
 echo "Generating flamegraph"
 OUTPUT=tmp/prof.svg
 
-stackcollapse.pl tmp/out.stacks | flamegraph.pl > $OUTPUT
+stackcollapse.pl $stacks | flamegraph.pl > $OUTPUT
 echo "Done, output in $OUTPUT"
