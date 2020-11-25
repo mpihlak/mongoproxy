01b466c31711104d41e2d342b3636102a313f0dd
Martin Pihlak <martin.pihlak@starship.co>
2020-11-25 22:57:20 +0200

    Dummy decoder

--- envoy-wasm/src/codec.rs
+++ envoy-wasm/src/codec.rs
@@ -1,47 +1,19 @@
+use std::error::Error;
 use mongodb::{MsgHeader, MongoMessage};
 
-enum CodecState {
-    Initial,
-    HaveHeader,
-    Broken,
-}
-
 pub struct MongoDecoder {
-    state: CodecState,
-    buf: Vec<u8>,
-    hdr: Option<MsgHeader>,
 }
 
 impl MongoDecoder {
 
-    fn next_message(&mut self, buf: &[u8]) -> Option<(MsgHeader, MongoMessage)> {
-        self.buf.extend_from_slice(&buf);
-        
-        match self.state {
-            CodecState::Initial => {
-                if self.buf.len() >= mongodb::HEADER_LENGTH {
-                    self.state = CodecState::HaveHeader;
-                    // TODO: read the header
-                    None
-                } else {
-                    None
-                }
-            },
-            CodecState::HaveHeader => {
-                if let Some(ref hdr) = self.hdr {
-                    if self.buf.len() >= hdr.message_length {
-                        // TODO: Advance the buffer
-                        self.state = CodecState::Initial;
-                    }
-                } else {
-                    self.state = CodecState::Broken;
-                }
-                None
-            },
-            CodecState::Broken => {
-                return None
-            }
-        }
+    // Add this bunch of bytes to our buffer and return all the Mongo messages
+    // we can parse from that.
+    //
+    // Need to support multiple messages as we might get a bunch of them in the
+    // same buffer.
+    //
+    fn get_messages(&mut self, buf: &[u8]) -> Result<Vec<(MsgHeader, MongoMessage)>, Box<dyn Error>> {
+        Ok(Vec::new())
     }
 }
 
