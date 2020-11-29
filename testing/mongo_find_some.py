c4ee0aaf3263333bf1a43cb54052bd5dad09a8b1
Martin Pihlak <martin.pihlak@starship.co>
2020-11-29 11:26:05 +0200

    Pass uri on commandline

--- testing/mongo_find_some.py
+++ testing/mongo_find_some.py
@@ -3,21 +3,33 @@ import time
 import os
 import pymongo
 
-con = pymongo.MongoClient("mongodb://localhost:27111")
+if len(sys.argv) > 1:
+    mongo_uri = sys.argv[1]
+else:
+    mongo_uri = "mongodb://localhost:27111"
+
+print(f"Connecting to {mongo_uri}")
+con = pymongo.MongoClient(mongo_uri)
 bigcollection = con['test']['bigcollection']
 
-print "Inserting some data"
+print("Cleaning up")
+bigcollection.drop()
+
+print("Inserting some data")
 for i in range(1000):
     bigcollection.insert_one({ "a": "bbbbbbbbbbbbbbbbbbbb", "b": "CCCCCCCCCCCCCCCCCC"})
 
-while True:
-    print "Now fetching it. ^C to exit"
-    count = 1000
+print("Running the fetch loop until ^C")
+try:
+    while True:
+        count = 1000
 
-    start = time.time()
-    for i in range(count):
-        for res in bigcollection.find({}).limit(1000):
-            pass
+        start = time.time()
+        for i in range(count):
+            for res in bigcollection.find({}).limit(1000):
+                pass
 
-    elapsed = time.time() - start
-    print "%d fetches done in %.2f seconds, %.3f / find" % (count, elapsed, elapsed/count)
+        elapsed = time.time() - start
+        print(f"{count} fetches done in {elapsed:.2f} seconds, {elapsed/count:.3f} / find")
+except KeyboardInterrupt:
+    pass
